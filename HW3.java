75d01bae586370c3a61dab340d0e272744bfb8fb
Terrence <1047930527@qq.com>
2015-11-17 19:29:24 -0800

    Audio Part Update.
    Audio Read-in & barcode generator functions done. Both works.

---
+++ HW3.java
@@ -0,0 +1,12 @@
+
+public class HW3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Wav wav=new Wav("sports2.wav");
+		wav.wavFile.display();
+		wav.barcode();
+		System.out.println("Program Done.");
+	}
+
+}
--- Wav.java
+++ Wav.java
@@ -1,7 +1,7 @@
 import java.io.*;
 
 public class Wav {
-	public int barcodeSize=1500;
+	public int barcodeSize=5000;
 	public WavFile wavFile;
 	public Wav(String file) {
 		try {
@@ -17,7 +17,6 @@ public class Wav {
 	public int[] barcode() {
 		final int windowSize=(int)Math.ceil((double)wavFile.getNumFrames()/barcodeSize)*wavFile.getNumChannels();
 		final int bufferSize=windowSize*wavFile.getNumChannels();
-		final int scale=(int) (Long.MAX_VALUE >> (64 - wavFile.getValidBits()));
 		double[] buffer=new double[bufferSize];
 		int[] code=new int[barcodeSize];
 		int framesRead;
@@ -35,18 +34,13 @@ public class Wav {
 			}
 			double sum=0;
 			for(int j=0;j<framesRead;j++) {
-				buffer[j]=(buffer[j]+1)*(scale-1);
-				if (buffer[j]<1)
-					buffer[j]=1;
-				buffer[j]=10*(17+Math.log10(buffer[j]));
-				sum+=buffer[j];
-				//System.out.println(Math.floor(buffer[j]));
+				sum+=Math.abs(buffer[j]);
+				//System.out.println(Math.abs(buffer[j]));
 			}
 			sum/=framesRead*wavFile.getNumChannels();
-			code[i]=(int) (((sum-170)/(10*(Math.log10(2*(scale-1))+17)-170))*255);
-			System.out.println(sum+"\t"+code[i]);
+			code[i]=(int)(sum*255);
+			System.out.println(code[i]);
 		}
-		
 		return code;
 	}
 	
